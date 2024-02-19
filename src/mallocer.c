23b5dd9f147b05236f085a52ef68fae1b14e3ed4
Wolf Hendrikse <whendrik@z1t6c1.42bangkok.com>
2024-02-19 15:39:10 +0700

    Final Version

---
+++ src/mallocer.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mallocer.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: whendrik <whendrik@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 14:12:14 by whendrik          #+#    #+#             */
+/*   Updated: 2023/11/09 17:45:00 by whendrik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philosopher.h"
+
+bool	malloc_mutex(int size, t_var *var)
+{
+	int	i;
+
+	i = 0;
+	var->mutex = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t) * size);
+	if (!(var->mutex))
+		return (false);
+	while (i < size)
+	{
+		pthread_mutex_init(&var->mutex[i], NULL);
+		i++;
+	}
+	var->lock = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+	if (!(var->lock))
+		return (false);
+	pthread_mutex_init(var->lock, NULL);
+	return (true);
+}
+
+bool	mallocer(int size, t_var *var)
+{
+	var->philo = (t_philo *)malloc(sizeof(t_philo) * size);
+	if (!var->philo)
+		return (false);
+	var->fork = (int *)malloc(sizeof(int) * size);
+	if (!var->fork)
+		return (false);
+	var->thd = (pthread_t *)malloc(sizeof(pthread_t) * size);
+	if (!var->thd)
+		return (false);
+	if (!(malloc_mutex(size, var)))
+		return (false);
+	var->is_alive = 1;
+	return (true);
+}
---
+++ src/utilities.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utilities.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: whendrik <whendrik@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/04 17:44:28 by whendrik          #+#    #+#             */
+/*   Updated: 2023/11/21 20:36:38 by whendrik         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/philosopher.h"
+
+void	free_all(t_var *var, int size)
+{
+	int	i;
+
+	i = 0;
+	free(var->philo);
+	free(var->thd);
+	free(var->fork);
+	while (i < size)
+		pthread_mutex_destroy(&var->mutex[i++]);
+	free(var->mutex);
+	pthread_mutex_destroy(var->lock);
+	free(var->lock);
+}
+
+int	print_msg(char *string, t_philo *philo)
+{
+	pthread_mutex_lock(philo->lock);
+	if (*philo->is_alive == 1)
+	{
+		philo->timeof = get_time();
+		printf(string,
+			(philo->timeof - philo->times.start) / 1000, philo->philo_id);
+		pthread_mutex_unlock(philo->lock);
+		return (1);
+	}
+	pthread_mutex_unlock(philo->lock);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	res;
+	int	i;
+	int	sign;
+
+	res = 0;
+	i = 0;
+	sign = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (str[i] <= '9' && str[i] >= '0')
+	{
+		res = (res * 10) + (str[i] - '0');
+		i++;
+	}
+	return (res * sign);
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
